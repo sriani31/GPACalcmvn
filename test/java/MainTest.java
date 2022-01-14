462dbe6a0ffff52cfcb0f3135b1256fcd58174d5
sriani31 <animesh.n.you@gmail.com>
2022-01-14 14:51:19 +0530

    Initial commit

---
+++ test/java/MainTest.java
@@ -0,0 +1,16 @@
+import org.junit.Test;
+import org.mockito.Mockito;
+
+import java.util.Scanner;
+
+
+class MainTest {
+
+
+
+    @Test
+    void main() {
+        Scanner mockScanner = Mockito.mock(Scanner.class);
+        //when(mockScanner.nextLine()).
+    }
+}
